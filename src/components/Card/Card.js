c93a3e2616d13eb279987979a6b029feeefec813
parth <srivastavaparth2016@gmail.com>
2018-03-04 22:37:20 +0530

    changes in esling for card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 import style from './Card.style';
 
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-    };
-  }
-
-  render() {
-    return (
-      <View style={style.CardFlex}>
-        <View style={style.CardTop}>
-          <Text style={style.CardWelcome}>Welcome</Text>
-          <Text style={style.CardTo}>to</Text>
-          <Text style={style.CardQuizzy}>Quizzy!</Text>
-        </View>
-        <View style={style.CardBottom}>
-          <Text style={style.CardLogin}>Login</Text>sdadasd
-          <Text style={style.CardUsername}>UserName</Text>
-          <TextInput style={style.CardInput} onChangeText={event => this.props.nameUser(event)} />
-          <View style={style.CardTouchView}><TouchableOpacity style={style.CardButton} onPress={() => this.props.onClick()}><Text>Login</Text></TouchableOpacity>
-          </View>
-        </View>
+const Card = props => (
+  <View style={style.CardFlex}>
+    <View style={style.CardTop}>
+      <Text style={style.CardWelcome}>Welcome</Text>
+      <Text style={style.CardTo}>to</Text>
+      <Text style={style.CardQuizzy}>Quizzy!</Text>
+    </View>
+    <View style={style.CardBottom}>
+      <Text style={style.CardLogin}>Login</Text>sdadasd
+      <Text style={style.CardUsername}>UserName</Text>
+      <TextInput style={style.CardInput} onChangeText={event => props.nameUser(event)} />
+      <View style={style.CardTouchView}><TouchableOpacity style={style.CardButton} onPress={() => props.onClick()}><Text>Login</Text></TouchableOpacity>
       </View>
-    );
-  }
-}
+    </View>
+  </View>
+);
 
+Card.propTypes = {
+  nameUser: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 export default Card;
